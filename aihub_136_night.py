d0da11ccbd6fc5a563fd3a7210778e34df3ea547
Polar <tpwns73@gmail.com>
2024-07-24 16:19:28 +0900

    136 데이터 parser

---
+++ aihub_136_night.py
@@ -0,0 +1,62 @@
+import os
+import shutil
+import json
+
+
+def convert_annotation_to_yolo_format(annotation, image_width, image_height):
+    yolo_annotation = []
+    
+    for ann in annotation:
+        category_id = ann["property"]["category_id"]
+        xmin = ann["bndbox"]["xmin"]
+        ymin = ann["bndbox"]["ymin"]
+        xmax = ann["bndbox"]["xmax"]
+        ymax = ann["bndbox"]["ymax"]
+        
+        x_center = (xmin + xmax) / 2
+        y_center = (ymin + ymax) / 2
+        width = xmax - xmin
+        height = ymax - ymin
+        
+        yolo_annotations.append(f"{category_id} {x_center/image_width} {y_center/image_height} {width/image_width} {height/image_height}")
+        
+    return yolo_annotations
+
+
+def main(source_dir, target_dir):
+    os.makedir(target_dir + "/images", exist_ok=True)
+    os.makedir(target_dir + "/labels", exist_ok=True)
+    
+    for root, dirs, files in os.walk(source_dir):
+        for file in files:
+            if file.endswith(".json"):
+                with open(os.path.join(root, file), "r") as f:
+                    data = json.load(f)
+                
+                image_info = data['image']
+                annotations = data['annotation']
+                image_width = image_info['width']
+                image_height = image_info['height']
+                image_file_name = image_info['file_name']
+                
+                image_path = os.path.join(root, image_file_name)
+                if os.path.exists(image_path):
+                    shutil.copy(image_path, os.path.join(target_dir, "images", image_file_name))
+                
+                yolo_annotations = convert_annotation_to_yolo_format(annotations, image_width, image_height)
+                label_filename = image_file_name.replace(".jpg", ".txt")
+                label_path = os.path.join(target_dir, 'labels', label_filename)
+                
+                with open(label_path, "w") as f:
+                    for line in yolo_annotations:
+                        f.write(line + "\n")
+                        
+    print("Transformation completed.")
+    
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Convert AIHub 136 Night dataset to YOLO format")
+    parser.add_argument("--source_dir", type=str, required=True, help="Path to the AIHub 136 Night dataset")
+    parser.add_argument("--target_dir", type=str, required=True, help="Path to the target directory")
+    
+    args = parser.parse_args()
+    main(ars.source_dir, args.target_dir)
