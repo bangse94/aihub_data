953725fd5322568d4f811fa09266d1615fbb4553
Polar <tpwns73@gmail.com>
2024-07-26 09:30:24 +0900

    json mismatch check

--- aihub_136_night.py
+++ aihub_136_night.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import json
+import argparse
 
 
 def convert_annotation_to_yolo_format(annotation, image_width, image_height):
@@ -24,19 +25,19 @@ def convert_annotation_to_yolo_format(annotation, image_width, image_height):
 
 
 def main(source_dir, target_dir):
-    os.makedir(target_dir + "/images", exist_ok=True)
-    os.makedir(target_dir + "/labels", exist_ok=True)
+    os.makedirs(target_dir + "/images", exist_ok=True)
+    os.makedirs(target_dir + "/labels", exist_ok=True)
     
     for root, dirs, files in os.walk(source_dir):
         for file in files:
             if file.endswith(".json"):
                 with open(os.path.join(root, file), "r") as f:
                     data = json.load(f)
-                
+                print(f"{root}, {file}")
                 image_info = data['image']
                 annotations = data['annotation']
-                image_width = image_info['width']
-                image_height = image_info['height']
+                image_width = image_info['size']['width']
+                image_height = image_info['size']['height']
                 image_file_name = image_info['file_name']
                 
                 image_path = os.path.join(root, image_file_name)
@@ -59,4 +60,4 @@ if __name__ == "__main__":
     parser.add_argument("--target_dir", type=str, required=True, help="Path to the target directory")
     
     args = parser.parse_args()
-    main(ars.source_dir, args.target_dir)
+    main(args.source_dir, args.target_dir)
