1dd22af0ae5b7470b27333dd614e812896255bbe
Polar <tpwns73@gmail.com>
2024-08-05 16:22:41 +0900

    change mod

--- unzip_labels.sh
+++ unzip_labels.sh
@@ -13,7 +13,7 @@ echo "[Train]All files unzipped to $DEST_DIR"
 
 # Valid set
 SOURCE_DIR="/data/aihub/224.멀티_영상_동일_상황_및_객체_식별_데이터/01-1.정식개방데이터/Validation/02.라벨링데이터"
-DEST_DIR="/data/aihub/day_data/valid/labls"
+DEST_DIR="/data/aihub/day_data/valid/labels"
 
 for file in "$SOURCE_DIR"/*.zip; do
 	echo "Unzipping $file to $DEST_DIR"
